69a062e78591b9de115f19c8e46245e36694e4a3
calvin <ctmichele@gmail.com>
2024-12-01 20:12:09 -0900

    main

--- MICHELE_problem_3/main.cpp
+++ MICHELE_problem_3/main.cpp
@@ -1,5 +1,13 @@
-
+#include <iostream>
+#include "include/prisoner.h"
+#include "include/linkedlist.h"
 
 int main(){
+	LinkedList deathRow;
+
+	for( int i=0; i<12; i++ ){
+		deathRow.add(i);
+	}
+
 	return 0;
 }
